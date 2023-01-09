e4493910d75c1b03a696cf3ab0d0a9dbf7a7fe04
Justus <justus.onyancha@moringaschool.com>
2023-01-09 15:13:53 +0300

    add changes in index.js file to showcase Dom nodes

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 console.log("test linking of js");
+
+// const p = document.createElement('p');
+
+// p.textContent = "Hello Justus";
+// document.body.appendChild(p);
+
+// for (let i = 0; i < 3; i++){
+//     const li = document.createElement(li);
+//     li.textContent = 
+// }
+
+
+// DOM SIZES
+
+const listItems = document.querySelector('ol');
+
+console.log(listItems);
